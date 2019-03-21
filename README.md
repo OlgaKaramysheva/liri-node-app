d26158fbff337acfb7584f1914577d97894db968
olgakaramyseva <olgakaramyseva@mac.com>
2019-03-20 23:49:37 -0700

    changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # liri-node-app
+
+This program searches for songs on Spotify.
--- liri.js
+++ liri.js
@@ -1,5 +1,40 @@
 require("dotenv").config();
-
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
+const command = process.argv.slice(2, 3).join("");
+const song = process.argv.slice(3).join(" ");
+
+function spotifySong() {
+    spotify
+        .search({ type: 'track', query: song })
+        .then(function (response) {
+
+            let items = response.tracks.items;
+
+            // for each index in items array
+            for (item in items) {
+                
+                let album = items[item].album;
+                let artists = album.artists;
+                let name = items[item].name;
+                let url = items[item].preview_url;
+
+                console.log("album: " + album.name);
+                console.log("name: " + name);
+                console.log("url: " + url);
+
+                for (i in artists) {
+                    console.log("artist: " + artists[i].name);
+                }
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+if (command === "spotify-this-song") {
+    spotifySong();
+}
